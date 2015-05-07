59fb7dc754d97afc56d7fb745494b82bafe953f3
Alex Fontenot <guiri.alien@gmail.com>
2015-05-06 21:54:41 -0500

    Now computes eigen faces, kind of.

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 using namespace Eigen;
 using namespace std;
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 320;
+const int SCREEN_HEIGHT = 243;
 
 
 int main( int argc, char* args[] )
@@ -96,10 +96,32 @@ int main( int argc, char* args[] )
         float average = sum/numFaces;
         psi(i, 0) = average;
     }
+
+    //Mean subtract all of the faces
+    MatrixXd A(w*h, numFaces);
+    for(int k = 0; k < numFaces; k++) {
+        for(int j = 0; j < h; j++) {
+            for(int i = 0; i < w; i++) {
+                A(j*w + i, k) = m(j*w + i, k) - psi(j*w + i, 0);
+            }
+        }
+    }
+
+    MatrixXd S(numFaces, numFaces);
+    S = A.transpose() * A;
+
+    EigenSolver<MatrixXd> es(S);
+    //cout << es.eigenvectors() << endl << es.eigenvalues() << endl;
+    
+    int eig = atoi(args[1]);
+    //Compute the first eigenface?
+    MatrixXd EigFace1(w*h, 1);
+    EigFace1 =  (A*(es.eigenvectors().col(eig).real())).cast<double>();
+    //Draw the first eigen face
     for(int x = 0; x < w; x++){
         for(int y = 0; y< h; y++) {
             int i = (y*w) + x;
-            int c = psi(i, 0);
+            int c = EigFace1(i, 0);
             SDL_SetRenderDrawColor(renderer, c, c, c, 255);
             SDL_RenderDrawPoint(renderer, x, y);
         }
@@ -108,14 +130,14 @@ int main( int argc, char* args[] )
     SDL_RenderPresent(renderer);
     SDL_Delay( 4000 );
 
-    SDL_Surface* screenshot = SDL_CreateRGBSurface(0, SCREEN_WIDTH, 
+    /*SDL_Surface* screenshot = SDL_CreateRGBSurface(0, SCREEN_WIDTH, 
       SCREEN_HEIGHT, 32, 0x00ff0000, 0x0000ff00, 0x000000ff, 0xff000000);
     if(screenshot) {
         SDL_RenderReadPixels(renderer, NULL, SDL_GetWindowPixelFormat(window),
           screenshot->pixels, screenshot->pitch);
         SDL_SaveBMP(screenshot, "Screenshot.bmp");
         SDL_FreeSurface(screenshot);
-    }
+    }*/
 
 
     SDL_DestroyWindow( window );
